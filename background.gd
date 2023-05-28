12c01b944b7d9078e50e2814bda828a640867267
otDan <otdanofficial@gmail.com>
2023-05-28 14:31:03 +0200

    feat: Parallax stars

--- background.gd
+++ background.gd
@@ -3,13 +3,12 @@ extends ParallaxBackground
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	generate_stars(100, Vector2(0.5, 0.75), Rect2(Vector2(0, 0), DisplayServer.screen_get_size()))
-	pass # Replace with function body.
+	generate_stars($FarStars, 150, Vector2(0.5, 0.75), Rect2(Vector2(0, 0), DisplayServer.screen_get_size()))
+	generate_stars($Stars, 70, Vector2(1, 1.25), Rect2(Vector2(0, 0), DisplayServer.screen_get_size()))
+	generate_stars($CloseStars, 20, Vector2(2, 2.5), Rect2(Vector2(0, 0), DisplayServer.screen_get_size()))
 
 
-func generate_stars(star_count: int, star_size_range: Vector2, spawn_area: Rect2):
-	var parallaxLayer := $ParallaxLayer  # Reference to the ParallaxLayer node
-	
+func generate_stars(parallaxLayer: ParallaxLayer, star_count: int, star_size_range: Vector2, spawn_area: Rect2):
 	for i in range(star_count):
 		var starInstance := Star.new()
 		starInstance.size = randf_range(star_size_range.x, star_size_range.y)

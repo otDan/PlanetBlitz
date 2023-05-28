c42dbde2ba6af9b96997699db9a24a563dd365c8
otDan <otdanofficial@gmail.com>
2023-05-28 06:16:38 +0200

    feat: Planet movement

--- player.gd
+++ player.gd
@@ -4,6 +4,10 @@ var thruster: Sprite2D
 @export var radius: float = 150.0  # Adjust the radius as needed
 @export var targetRotation: float = PI  # Set the target rotation for left side
 @export var rotationSpeed: float = 30.0  # Adjust the rotation speed as needed
+@export var acceleration: float = 150.0  # Adjust the acceleration as needed
+@export var deceleration: float = 1.0  # Adjust the deceleration as needed
+
+var velocity: Vector2 = Vector2.ZERO
 
 func _ready():
 	thruster = $Planet/Thruster
@@ -27,6 +31,14 @@ func _process(delta):
 
 	thruster.rotation = lerp_angle(thruster.rotation, targetRotation, rotationSpeed * delta)
 	thruster.position = Vector2(radius, 0).rotated(thruster.rotation) + $Planet.position
+
+	if inputDirection != Vector2.ZERO:
+		var desiredVelocity = (thruster.position - $Planet.position).normalized() * acceleration
+		velocity = velocity.lerp(desiredVelocity, delta)
+	else:
+		velocity = velocity.lerp(Vector2.ZERO, deceleration * delta)
+
+	position -= velocity * delta
 
 func lerp_angle(start: float, end: float, weight: float) -> float:
 	var difference = fmod(end - start + 3 * PI, 2 * PI) - PI

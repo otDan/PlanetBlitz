3866765797b6b553c3320f9f09819bfe06ffa6d3
otDan <otdanofficial@gmail.com>
2023-05-29 06:30:24 +0200

    feat: Star blinking

--- star.gd
+++ star.gd
@@ -1,9 +1,22 @@
 extends Node2D
 class_name Star
-	
+
 var size = 1
+var brightness = 1.0
+var blinkSpeed = 1.0
+var timer = 0.0
+var blinkOffset = 0.0
+
+
+func _ready():
+	blinkOffset = randf() * 100
 
 
 func _draw():
-	var starSize = size * 2
-	draw_circle(Vector2.ZERO, starSize, Color(1, 1, 1))
+	draw_circle(Vector2.ZERO, size, Color(1, 1, 1) * brightness)
+
+
+func _process(delta):
+	timer += delta
+	brightness = (sin((timer * blinkSpeed) + blinkOffset) + 1) / 2
+	queue_redraw()

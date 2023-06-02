e9210d3862e8268f77d98e68be84f28cca5caba8
otDan <otdanofficial@gmail.com>
2023-06-02 09:57:22 +0200

    feat: More debug displays

--- background/background.gd
+++ background/background.gd
@@ -8,7 +8,6 @@ func _ready():
 	generate_stars($FarStars, 700, Vector2(1, 1.25), gen_area)
 	generate_stars($Stars, 390, Vector2(1, 2.55), gen_area)
 	generate_stars($CloseStars, 85, Vector2(1.5, 5), gen_area)
-#	generate_stars($CloseStars, 1, Vector2(10, 15), gen_area)
 
 
 func generate_stars(parallaxLayer: ParallaxLayer, star_count: int, star_size_range: Vector2, spawn_area: Rect2):
@@ -21,17 +20,3 @@ func generate_stars(parallaxLayer: ParallaxLayer, star_count: int, star_size_ran
 		starInstance.position = starPosition
 		
 		parallaxLayer.add_child(starInstance)
-
-
-#func _process(delta):	
-#	var stars = []
-#	stars.append_array($FarStars.get_children())
-#	stars.append_array($Stars.get_children())
-#	stars.append_array($CloseStars.get_children())
-	
-#	for star in stars:
-#		var star_distance = camera.global_position.distance_to(star.global_position)
-#		if star_distance < 100:
-#		if star.to_update:
-#			star.queue_redraw()
-#		print("updating: %s" % [str(star_distance)])
--- debug.gd
+++ debug.gd
@@ -3,6 +3,9 @@ extends Node
 @export var player: Node2D
 
 @export var fps: Label
+@export var memory: Label
+
+@export var position: Label
 @export var speed: Label
 
 
@@ -12,8 +15,11 @@ func _ready():
 
 func _process(delta):
 	fps.text = "Fps: " + str(Engine.get_frames_per_second())
+	memory.text = "Memory: " + str(Performance.get_monitor(Performance.MEMORY_STATIC))
+	
+	position.text = "Position: " + str(round(player.global_position))
 	speed.text = "Speed: " + str(roundf(player.velocity.length()))
 
 
 func _on_randomize_button_pressed():
-	player.get_node("Planet")._ready()
+	player.get_node("Planet/Planet")._ready()
